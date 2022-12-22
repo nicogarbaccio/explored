d5834b3fa7cd3377d01bae8e77af2fd03304577f
Nico Garbaccio <garbaccio20@gmail.com>
2022-12-22 09:35:14 -0500

    Installed Tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    // preflight: false,
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'yellow': '#F49D37',
+        'purple': '#140F2D',
+        'skobeloff': '#297373',
+        'queenblue': '#456990',
+        'charcoal': '#294C60',
+        'red': {
+          100: '#EF767A',
+          200: '#BA3B46'
+        }
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+};
